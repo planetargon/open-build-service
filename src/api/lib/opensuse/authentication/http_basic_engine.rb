90a87c442095721989b82ce9d3277860a897b185
Carlos Rodriguez <carlos@eddorre.com>
2012-11-05 13:43:57 -0800

    Cleaned up code for HTTP Basic engine. IN-6

--- src/api/lib/opensuse/authentication/http_basic_engine.rb
+++ src/api/lib/opensuse/authentication/http_basic_engine.rb
@@ -12,20 +12,18 @@ module Opensuse
       end
 
       def authenticate
-        user = nil
         read_only_hosts = configuration['read_only_hosts'] || []
         read_only_hosts << configuration['webui_host'] if configuration['webui_host'] # This was used in config files until OBS 2.1
 
         if read_only_hosts.include?(environment['REMOTE_HOST']) || read_only_hosts.include?(environment['REMOTE_ADDR'])
-          user_agent = environment['HTTP_USER_AGENT']
-          if user_agent.match(/^obs-webui/) || user_agent.match(/^obs-software/)
+          if environment['HTTP_USER_AGENT'].match(/^(obs-webui|obs-software)/)
            return User.find_by_login("_nobody_")
           end
         else
-          logger.send :error, "Anonymous configured, but #{read_only_hosts.inspect} does not inclide '#{environment['REMOTE_HOST']} '#{environment['REMOTE_ADDR']}'}'"
+          logger.send :error, "Anonymous configured, but #{read_only_hosts.inspect} does not include '#{environment['REMOTE_HOST']} '#{environment['REMOTE_ADDR']}'}'"
         end
 
-        # LOG "No authentication string was sent"
+        logger.send :info, "No authentication string was sent"
         return [nil, "Authentication required"]
       end
     end
