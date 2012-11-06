915a48542190c5ffa68cd5b4f8c4e61b7c9f7046
Carlos Rodriguez <carlos@eddorre.com>
2012-11-05 17:47:40 -0800

    Fixed LDAP Engine code. IN-6

--- src/api/lib/opensuse/authentication/ldap_engine.rb
+++ src/api/lib/opensuse/authentication/ldap_engine.rb
@@ -20,30 +20,34 @@ module Opensuse
         if authorization && authorization[0] == "Basic"
           login, password = Base64.decode64(authorization[1]).split(':', 2)[0..1]
 
-          user_login = login
+          @user_login = login
 
           # Set password to the empty string in case no password is transmitted in the auth string
           password ||= ""
 
           # Disallow empty passwords to prevent LDAP lockouts
-          return [nil, "User '#{login} did not provide a password'"] if !password || password == ""
+          return [nil, "User '#{login}' did not provide a password"] if !password || password == ""
 
           user = nil
           ldap_info = nil
 
           begin
             #logger.debug( "Using LDAP to find #{login}" )
-            ldap_info, ldap_user_info = User.find_with_ldap(login, passwd)
+            ldap_info = User.find_with_ldap(login, password)
+            @ldap_user_info = ldap_info
           rescue LoadError
             loggersend :warn, "ldap_mode selected but 'ruby-ldap' module not installed."
-          rescue Exception
+          rescue Exception => exception
             logger.send :debug, "#{login} not found in LDAP."
           end
 
-          if ldap_info
+          if ldap_info.present?
             # We've found an LDAP authenticated user - find or create an OBS user database entry
             user = User.find_by_login(login)
-            user.update_attributes(:email => ldap_info[0]) if user
+            if user
+              user.update_attributes(:email => ldap_info[0])
+              user.reload
+            end
           end
 
           if user
@@ -51,6 +55,9 @@ module Opensuse
           else
             [nil, "Unknown user '#{login}' or invalid password"]
           end
+        else
+          logger.send :debug, "No authentication string was sent"
+          [nil, "Authentication required"]
         end
       end
     end
