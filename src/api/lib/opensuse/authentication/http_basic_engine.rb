4cac3776e3d7bce558f0f8fc7578629e988cf760
Carlos Rodriguez <carlos@eddorre.com>
2012-11-09 12:19:19 -0800

    HTTP Basic Authentication engine now checks user credentials if allow anonymous is not configured. IN-6

--- src/api/lib/opensuse/authentication/http_basic_engine.rb
+++ src/api/lib/opensuse/authentication/http_basic_engine.rb
@@ -2,6 +2,7 @@ module Opensuse
   module Authentication
     class HttpBasicEngine
       include Opensuse::Authentication::Logger
+      include Opensuse::Authentication::HttpHeader
 
       attr_reader :configuration, :environment
       attr_accessor :user_login
@@ -12,19 +13,39 @@ module Opensuse
       end
 
       def authenticate
-        read_only_hosts = Array(configuration['read_only_hosts']) || []
-        read_only_hosts << configuration['webui_host'] if configuration['webui_host'] # This was used in config files until OBS 2.1
+        authorization = extract_authorization_from_header
 
-        if read_only_hosts.include?(environment['REMOTE_HOST']) || read_only_hosts.include?(environment['REMOTE_ADDR'])
-          if environment['HTTP_USER_AGENT'] && environment['HTTP_USER_AGENT'].match(/^(obs-webui|obs-software)/)
-           return User.find_by_login("_nobody_")
+        logger.send :debug, "AUTH: #{authorization.inspect}"
+
+        user = nil
+
+        if authorization && authorization[0] == "Basic"
+          login, password = Base64.decode64(authorization[1]).split(':', 2)[0..1]
+
+          @user_login = login
+
+          read_only_hosts = Array(configuration['read_only_hosts']) || []
+          read_only_hosts << configuration['webui_host'] if configuration['webui_host'] # This was used in config files until OBS 2.1
+
+          if read_only_hosts.include?(environment['REMOTE_HOST']) || read_only_hosts.include?(environment['REMOTE_ADDR'])
+            if environment['HTTP_USER_AGENT'] && environment['HTTP_USER_AGENT'].match(/^(obs-webui|obs-software)/)
+             user = User.find_by_login("_nobody_")
+            end
+          else
+            logger.send :error, "Anonymous configured, but #{read_only_hosts.inspect} does not include '#{environment['REMOTE_HOST']} '#{environment['REMOTE_ADDR']}'}'"
+          end
+
+          user = User.find_with_credentials(login, password)
+
+          if user.nil?
+            [nil, "Unknown user '#{login}' or invalid password"]
+          else
+            user
           end
         else
-          logger.send :error, "Anonymous configured, but #{read_only_hosts.inspect} does not include '#{environment['REMOTE_HOST']} '#{environment['REMOTE_ADDR']}'}'"
+          logger.send :debug, "No authentication string was sent"
+          [nil, "Authentication required"]
         end
-
-        logger.send :info, "No authentication string was sent"
-        return [nil, "Authentication required"]
       end
     end
   end
