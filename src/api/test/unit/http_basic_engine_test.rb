28400c8106edc9b2fb3004d16c200e180c531faf
Carlos Rodriguez <carlos@eddorre.com>
2012-11-13 10:17:29 -0800

    [api] Updated logic on HTTP Basic Authentication engine with tests. IN-6

--- src/api/test/unit/http_basic_engine_test.rb
+++ src/api/test/unit/http_basic_engine_test.rb
@@ -33,10 +33,11 @@ class HttpBasicEngineTest < ActiveSupport::TestCase
     assert_equal nil, auth_engine.authenticate.first
   end
 
-  def returns_a_nobdy_user_when_the_user_agent_is_obs_webui
+  def test_returns_a_nobdy_user_when_the_user_agent_is_obs_webui
     user = User.find_by_login("_nobody_")
 
     @configuration['read_only_hosts'] = 'test_host'
+    @configuration['allow_anonymous'] = true
     @environment['REMOTE_HOST'] = 'test_host'
     @environment['HTTP_USER_AGENT'] = 'obs-webui'
     @environment['REMOTE_HOST'] = 'test_host'
@@ -46,10 +47,11 @@ class HttpBasicEngineTest < ActiveSupport::TestCase
     assert_equal user, auth_engine.authenticate
   end
 
-  def returns_a_nobdy_user_when_the_user_agent_is_obs_software
+  def test_returns_a_nobdy_user_when_the_user_agent_is_obs_software
     user = User.find_by_login("_nobody_")
 
     @configuration['read_only_hosts'] = 'test_host'
+    @configuration['allow_anonymous'] = true
     @environment['REMOTE_HOST'] = 'test_host'
     @environment['HTTP_USER_AGENT'] = 'obs-software'
     @environment['REMOTE_HOST'] = 'test_host'
@@ -57,5 +59,41 @@ class HttpBasicEngineTest < ActiveSupport::TestCase
     auth_engine = Opensuse::Authentication::HttpBasicEngine.new(@configuration, @environment)
 
     assert_equal user, auth_engine.authenticate
+  end
+
+  def test_returns_a_database_user_when_allow_anonymous_is_false
+    @configuration['allow_anonymous'] = false
+
+    user = User.create(:login => "Joe", :password => "MyPassword", :password_confirmation => "MyPassword", :email => "joe@example.com")
+    @environment['X-HTTP_AUTHORIZATION'] = "Basic #{Base64.encode64('Joe:MyPassword')}"
+
+    auth_engine = Opensuse::Authentication::HttpBasicEngine.new(@configuration, @environment)
+
+    assert_equal user, auth_engine.authenticate
+  end
+
+  def test_returns_a_nobody_user_when_allow_anonymous_is_true_but_there_is_a_database_user
+    @configuration['allow_anonymous'] = true
+    @configuration['read_only_hosts'] = 'test_host'
+    @environment['REMOTE_HOST'] = 'test_host'
+    @environment['HTTP_USER_AGENT'] = 'obs-software'
+    @environment['REMOTE_HOST'] = 'test_host'
+
+    user = User.create(:login => "Joe", :password => "MyPassword", :password_confirmation => "MyPassword", :email => "joe@example.com")
+    nobody = User.find_by_login("_nobody_")
+    @environment['X-HTTP_AUTHORIZATION'] = "Basic #{Base64.encode64('Joe:MyPassword')}"
+
+    auth_engine = Opensuse::Authentication::HttpBasicEngine.new(@configuration, @environment)
+
+    assert_equal nobody, auth_engine.authenticate
+  end
+
+  def test_returns_a_database_user_when_allow_anonymous_is_not_test
+     user = User.create(:login => "Joe", :password => "MyPassword", :password_confirmation => "MyPassword", :email => "joe@example.com")
+     @environment['X-HTTP_AUTHORIZATION'] = "Basic #{Base64.encode64('Joe:MyPassword')}"
+
+     auth_engine = Opensuse::Authentication::HttpBasicEngine.new(@configuration, @environment)
+
+     assert_equal user, auth_engine.authenticate
   end
 end
