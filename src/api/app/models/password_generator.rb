95b3d447b70055530f85e4ddc808d1d7b7a1ccbe
Carlos Rodriguez <carlos@eddorre.com>
2012-11-15 14:50:44 -0800

    Remove unused password. IN-6

--- src/api/app/models/password_generator.rb
+++ src/api/app/models/password_generator.rb
@@ -1,6 +1,6 @@
 class PasswordGenerator
   def self.generate_random_password
     chars = ["A".."Z","a".."z","0".."9"].collect { |r| r.to_a }.join
-    fakepw = (1..24).collect { chars[rand(chars.size)] }.pack("a"*24)
+    (1..24).collect { chars[rand(chars.size)] }.pack("a"*24)
   end
 end
