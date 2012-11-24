9b9f19129c17657bc3aefd8d37bc5ee4e2162fd7
Gary Blessington <garyblessington@gmail.com>
2012-11-24 15:16:20 -0800

    [api] Added new configuration options

---
+++ src/api/app/models/application_settings/ldap_servers.rb
@@ -0,0 +1,14 @@
+class ApplicationSettings::LdapServers < ApplicationSetting
+
+  def self.get
+    first || create!(:string_value => '')
+  end
+
+  def value
+   string_value
+  end
+
+  def value=(new_value)
+    self.string_value = new_value || ''
+  end
+end
