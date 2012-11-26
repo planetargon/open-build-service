e59e69ae45c57626d913f8be4b5a3a26558ecb58
Gary Blessington <garyblessington@gmail.com>
2012-11-26 08:15:59 -0800

    [api] Tweaks to application settings

--- src/api/app/controllers/application_settings_controller.rb
+++ src/api/app/controllers/application_settings_controller.rb
@@ -13,7 +13,7 @@ class ApplicationSettingsController < ApplicationController
       begin
         Rails.logger.debug "Updating #{ key } with #{ xml[key] }"
         application_setting = eval("ApplicationSettings::#{ key }.get")
-        application_setting.value = xml[key].blank? ? '' : xml[key]
+        application_setting.value = xml[key].blank? ? nil : xml[key]
         unless application_setting.save
           Rails.logger.debug "Error: #{ application_setting.errors.full_messages } #{ application_setting.inspect }"
           errors << application_setting.errors.full_messages.map { |message| "#{ key }#{ message.gsub('Integer', '').gsub('Boolean', '').gsub('String', '') }" }
