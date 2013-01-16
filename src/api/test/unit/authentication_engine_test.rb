cdce2eb8ae0671a444c32c734dcdb4b7ffe24a99
Carlos Rodriguez <carlos@eddorre.com>
2013-01-16 10:15:10 -0800

    [api] Renamed HTTP Basic Engine tests to match credentials engine test. Removed unused tests. IN-6

--- src/api/test/unit/authentication_engine_test.rb
+++ src/api/test/unit/authentication_engine_test.rb
@@ -6,119 +6,32 @@ class AuthenticationEngineTest < ActiveSupport::TestCase
     @environment = {}
   end
 
-  def test_crowd_engine_set_to_on
-    ApplicationSettings::AuthCrowdMode.set!(true)
-    ApplicationSettings::AuthCrowdServer.set!('192.168.1.1')
-    ApplicationSettings::AuthCrowdAppName.set!('obs-api')
-    ApplicationSettings::AuthCrowdAppPassword.set!('password')
-
-    @environment['Authorization'] = 'Joe'
-    auth_engine = Opensuse::Authentication::AuthenticationEngine.new(@config, @environment)
-    assert_equal "Opensuse::Authentication::CrowdEngine", auth_engine.engine.class.to_s
-  end
-
   def test_ichain_engine_set_to_on
-    ApplicationSettings::AuthIchainMode.set!('on')
+    @config['ichain_mode'] = :on
 
     auth_engine = Opensuse::Authentication::AuthenticationEngine.new(@config, @environment)
     assert_equal "Opensuse::Authentication::IchainEngine", auth_engine.engine.class.to_s
   end
 
   def test_ichain_engine_set_to_simulate
-    ApplicationSettings::AuthIchainMode.set!('simulate')
+    @config['ichain_mode'] = :simulate
+
     auth_engine = Opensuse::Authentication::AuthenticationEngine.new(@config, @environment)
     assert_equal "Opensuse::Authentication::IchainEngine", auth_engine.engine.class.to_s
   end
 
   def test_ichain_engine_set_to_off
-    ApplicationSettings::AuthIchainMode.set!('off')
+    @config['ichain_mode'] = :off
 
     auth_engine = Opensuse::Authentication::AuthenticationEngine.new(@config, @environment)
     assert_equal "NilClass", auth_engine.engine.class.to_s
   end
 
   def test_ichain_engine_set_to_nothing
-    ApplicationSettings::AuthIchainMode.set!('')
+    @config['ichain_mode'] = ''
 
     auth_engine = Opensuse::Authentication::AuthenticationEngine.new(@config, @environment)
     assert_equal "NilClass", auth_engine.engine.class.to_s
-  end
-
-  def test_http_basic_engine_x_http_authorization_header
-    ApplicationSettings::AuthAllowAnonymous.set!(true)
-
-    @environment['X-HTTP-Authorization'] = 'Joe'
-
-    auth_engine = Opensuse::Authentication::AuthenticationEngine.new(@config, @environment)
-    assert_equal "Opensuse::Authentication::CredentialsEngine", auth_engine.engine.class.to_s
-  end
-
-  def test_http_basic_engine_authorization_header
-    ApplicationSettings::AuthAllowAnonymous.set!(true)
-
-    @environment['Authorization'] = 'Joe'
-
-    auth_engine = Opensuse::Authentication::AuthenticationEngine.new(@config, @environment)
-    assert_equal "Opensuse::Authentication::CredentialsEngine", auth_engine.engine.class.to_s
-  end
-
-  def test_http_basic_engine_http_authorization_header
-    ApplicationSettings::AuthAllowAnonymous.set!(true)
-
-    @environment['HTTP_AUTHORIZATION'] = 'Joe'
-
-    auth_engine = Opensuse::Authentication::AuthenticationEngine.new(@config, @environment)
-    assert_equal "Opensuse::Authentication::CredentialsEngine", auth_engine.engine.class.to_s
-  end
-
-  def test_http_basic_engine_http_non_anonymous
-    ApplicationSettings::AuthAllowAnonymous.set!(false)
-
-    @environment['HTTP_AUTHORIZATION'] = 'Joe'
-
-    auth_engine = Opensuse::Authentication::AuthenticationEngine.new(@config, @environment)
-    assert_not_equal "Opensuse::Authentication::CredentialsEngine", auth_engine.engine.class.to_s
-  end
-
-  def test_ldap_engine_x_http_authorization_header_ldap_mode_on
-    ApplicationSettings::AuthAllowAnonymous.set!(false)
-    ApplicationSettings::LdapMode.set!(true)
-
-    @environment['X-HTTP-Authorization'] = 'Joe'
-
-    auth_engine = Opensuse::Authentication::AuthenticationEngine.new(@config, @environment)
-    assert_equal "Opensuse::Authentication::LdapEngine", auth_engine.engine.class.to_s
-
-  end
-
-  def test_ldap_engine_authorization_header_ldap_mode_on
-    ApplicationSettings::AuthAllowAnonymous.set!(false)
-    ApplicationSettings::LdapMode.set!(true)
-
-    @environment['Authorization'] = 'Joe'
-
-    auth_engine = Opensuse::Authentication::AuthenticationEngine.new(@config, @environment)
-    assert_equal "Opensuse::Authentication::LdapEngine", auth_engine.engine.class.to_s
-  end
-
-  def test_ldap_engine_http_authorization_header_ldap_mode_on
-    ApplicationSettings::AuthAllowAnonymous.set!(false)
-    ApplicationSettings::LdapMode.set!(true)
-
-    @environment['HTTP_AUTHORIZATION'] = 'Joe'
-
-    auth_engine = Opensuse::Authentication::AuthenticationEngine.new(@config, @environment)
-    assert_equal "Opensuse::Authentication::LdapEngine", auth_engine.engine.class.to_s
-  end
-
-  def test_ldap_engine_ldap_mode_off
-    ApplicationSettings::AuthAllowAnonymous.set!(false)
-    ApplicationSettings::LdapMode.set!(false)
-
-    @environment['HTTP_AUTHORIZATION'] = 'Joe'
-
-    auth_engine = Opensuse::Authentication::AuthenticationEngine.new(@config, @environment)
-    assert_not_equal "Opensuse::Authentication::LdapEngine", auth_engine.engine.class.to_s
   end
 
   def test_credentials_engine_x_http_authorization_header
@@ -129,19 +42,54 @@ class AuthenticationEngineTest < ActiveSupport::TestCase
   end
 
   def test_credentials_engine_authorization_header
-    ApplicationSettings::AuthAllowAnonymous.set!(false)
     @environment['Authorization'] = 'Joe'
 
     auth_engine = Opensuse::Authentication::AuthenticationEngine.new(@config, @environment)
     assert_equal "Opensuse::Authentication::CredentialsEngine", auth_engine.engine.class.to_s
   end
 
-  def test_credentials_engine_x_http_authorization_header
-    ApplicationSettings::AuthAllowAnonymous.set!(false)
+  def test_credentials_engine_http_authorization_header
     @environment['HTTP_AUTHORIZATION'] = 'Joe'
 
     auth_engine = Opensuse::Authentication::AuthenticationEngine.new(@config, @environment)
     assert_equal "Opensuse::Authentication::CredentialsEngine", auth_engine.engine.class.to_s
+  end
+
+  def test_ldap_engine_x_http_authorization_header_ldap_mode_on
+    @config['ldap_mode'] = :on
+
+    @environment['X-HTTP-Authorization'] = 'Joe'
+
+    auth_engine = Opensuse::Authentication::AuthenticationEngine.new(@config, @environment)
+    assert_equal "Opensuse::Authentication::LdapEngine", auth_engine.engine.class.to_s
+
+  end
+
+  def test_ldap_engine_authorization_header_ldap_mode_on
+    @config['ldap_mode'] = :on
+
+    @environment['Authorization'] = 'Joe'
+
+    auth_engine = Opensuse::Authentication::AuthenticationEngine.new(@config, @environment)
+    assert_equal "Opensuse::Authentication::LdapEngine", auth_engine.engine.class.to_s
+  end
+
+  def test_ldap_engine_http_authorization_header_ldap_mode_on
+    @config['ldap_mode'] = :on
+
+    @environment['HTTP_AUTHORIZATION'] = 'Joe'
+
+    auth_engine = Opensuse::Authentication::AuthenticationEngine.new(@config, @environment)
+    assert_equal "Opensuse::Authentication::LdapEngine", auth_engine.engine.class.to_s
+  end
+
+  def test_ldap_engine_ldap_mode_off
+    @config['ldap_mode'] = :off
+
+    @environment['HTTP_AUTHORIZATION'] = 'Joe'
+
+    auth_engine = Opensuse::Authentication::AuthenticationEngine.new(@config, @environment)
+    assert_not_equal "Opensuse::Authentication::LdapEngine", auth_engine.engine.class.to_s
   end
 
   def test_no_engine
