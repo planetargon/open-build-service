678188b61dfdd9f222ab171a9b7acc1cf818c4e0
Carlos Rodriguez <carlos@eddorre.com>
2013-01-16 21:17:52 -0800

    [api] In case HTTP_USER_AGENT doesn't match obs-webui or obs-software send a better error message. IN-6

--- src/api/lib/opensuse/authentication/anonymous_engine.rb
+++ src/api/lib/opensuse/authentication/anonymous_engine.rb
@@ -16,17 +16,22 @@ module Opensuse
 
         read_only_hosts = Array(configuration['read_only_hosts']) || []
         read_only_hosts << configuration['webui_host'] if configuration['webui_host'] # This was used in config files until OBS 2.1
+        return_message = ""
 
         if read_only_hosts.include?(environment['REMOTE_HOST']) || read_only_hosts.include?(environment['REMOTE_ADDR'])
           if environment['HTTP_USER_AGENT'] && environment['HTTP_USER_AGENT'].match(/^(obs-webui|obs-software)/)
             user = User.find_by_login('_nobody_')
+          else
+            logger.send :debug, "No authentication string was sent"
+            return_message = "Authentication required"
           end
         else
-          logger.send :error, "Anonymous configured, but #{read_only_hosts.join(', ')} does not include '#{environment['REMOTE_HOST']} '#{environment['REMOTE_ADDR']}'}'"
+          return_message = "Anonymous configured, but #{read_only_hosts.join(', ')} does not include '#{environment['REMOTE_HOST']} '#{environment['REMOTE_ADDR']}'}'"
+          logger.send :error, error_message
         end
 
         if user.nil?
-          [nil, "Anonymous configured, but #{read_only_hosts.join(', ')} does not include '#{environment['REMOTE_HOST']} '#{environment['REMOTE_ADDR']}'}'"]
+          [nil, return_message]
         else
           user
         end
